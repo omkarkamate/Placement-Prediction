7e115f4a72e8c5085d41eef1c3f14c748c09bd8c
omkarkamate <omkarkamate2004@gmail.com>
2026-03-09 18:33:26 +0530

    data preprocessing

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 from src.placement_prediction.looger import logging
 from src.placement_prediction.exception import Cu_Exception
-from src.placement_prediction.components.data_ingestion import Data_Ingestion
+from src.placement_prediction.components.data_preprocessing import Data_preprocessing
 import pandas as pd
 import sys
 if __name__=="__main__":
     try:
-        with open("artifacts/raw_data.csv","r") as f:
-            df=pd.read_csv(f)
+        # with open("artifacts/raw_data.csv","r") as f:
+        #     df=pd.read_csv(f)
         
-        print(df.head())
-        df.drop(["student_id","gender","coding_skill_score","aptitude_score","communication_skill_score","logical_reasoning_score","hackathons_participated","github_repos","linkedin_connections","mock_interview_score","attendance_percentage","attendance_percentage","backlogs","extracurricular_score","leadership_score","volunteer_experience","sleep_hours","study_hours_per_day"],axis=1,inplace=True)
-        print(df.columns)
+        # print(df.head())
+        # df.drop(["student_id","gender","coding_skill_score","aptitude_score","communication_skill_score","logical_reasoning_score","hackathons_participated","github_repos","linkedin_connections","mock_interview_score","attendance_percentage","attendance_percentage","backlogs","extracurricular_score","leadership_score","volunteer_experience","sleep_hours","study_hours_per_day"],axis=1,inplace=True)
+        # print(df.columns)
+
+        obj=Data_preprocessing()
+        print(obj.Data_Preprocess("artifacts/train_data.csv","artifacts/test_data.csv"))
+        
+
 
     except Exception as e:
         raise Cu_Exception(e,sys)
-
-
-
- #   Column                     Non-Null Count   Dtype  
----  ------                     --------------   -----  
- 0   age                        100000 non-null  int64  
- 1   cgpa                       100000 non-null  float64
- 2   branch                     100000 non-null  str    
- 3   college_tier               100000 non-null  str    
- 4   internships_count          100000 non-null  int64  
- 5   projects_count             100000 non-null  int64  
- 6   certifications_count       100000 non-null  int64  
- 7   communication_skill_score  100000 non-null  float64
- 8   backlogs                   100000 non-null  int64  
- 9   placement_status           100000 non-null  str    
- 10  salary_package_lpa         100000 non-null  float64
--- src/placement_prediction/components/data_preprocessing.py
+++ src/placement_prediction/components/data_preprocessing.py
@@ -1,19 +1,91 @@
-import pandas as pd
 from src.placement_prediction.looger import logging
 from src.placement_prediction.exception import Cu_Exception
-import sys
+import pandas as pd
 import os
+import sys
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
 import pickle
+import numpy as np
 
 
-class data_transformation_congig:
-    config_path=os.path.join("artifacts,preprocessing.pkl")
 
-class data_preprocessing:
+df=pd.read_csv("artifacts/raw_data.csv")
+
+class Data_Preprocessing_Config:
+    DPConfig=os.path.join("artifacts","Data_Preprocessing.pkl")
+    logging.info("Data Preprocessing config created")
+
+class Data_preprocessing:
     def __init__(self):
-        self.config=data_transformation_congig.config_path()
+        self.Config=Data_Preprocessing_Config()
     
-    def initiate_preprocessing(self):
+    def initiate_data_Preprocesiing(self):
+        try:
+
+            num_columns=["age","cgpa","internships_count","projects_count","certifications_count","communication_skill_score","backlogs"]
+            cat_columns=["branch","college_tier"]
+
+           
+
+            num_pipeline=Pipeline(steps=[
+                ("imputer",SimpleImputer(strategy="mean")),
+                ("scaler",StandardScaler())
+            ])
+
+            cat_pipeline=Pipeline(steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("Encoding",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+            ])
+
+
+            preprocessor=ColumnTransformer([
+                ("num",num_pipeline,num_columns),
+                ("cat",cat_pipeline,cat_columns)
+
+            ])
+
+            return preprocessor
+
+        except(Exception) as e:
+            raise Cu_Exception(e,sys)
+        
+    def Data_Preprocess(self,train_path,test_path):
+        try:
+            
+            train_data=pd.read_csv(train_path)
+            test_data=pd.read_csv(test_path)
+
+            preprocessor=self.initiate_data_Preprocesiing()
+
+            X_train=train_data.drop(columns=["placement_status","salary_package_lpa"])
+            y_train=train_data["placement_status"]
+            X_test=test_data.drop(columns=["placement_status","salary_package_lpa"])
+            y_test=test_data["placement_status"]
+
+            logging.info("data split in target columns")
+
+            X_train=preprocessor.fit_transform(X_train)
+            X_test=preprocessor.transform(X_test)
+
+            logging.info("preprocessing on train test data")
+
+            train_data=np.c_[X_train,np.array(y_train)]
+            test_data=np.c_[X_test,np.array(y_test)]
+
+            with open(self.Config.DPConfig,"wb") as f:
+                pickle.dump(preprocessor,f)
+
+            logging.info("pickle file created")
+
+            return train_data,test_data
         
 
-    
+
+
+        except(Exception) as e:
+            raise Cu_Exception(e,sys)
